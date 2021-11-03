15e6cb2fa1c9437ea1d53649d45897a72994e290
puneet56 <puneetdhiman56@gmail.com>
2021-11-03 11:47:05 +0530

    added rate-limiter

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const helmet = require('helmet');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
+const rateLimit = require('express-rate-limit');
+const app = express();
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+// app.set('trust proxy', 1);
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 200, // limit each IP to 100 requests per windowMs
+});
+
+//  apply to all requests
+app.use(limiter);
+
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
@@ -14,7 +28,6 @@ mongoose.connect(process.env.MONGO_URL, () => {
 	console.log('Connected to database');
 });
 
-const app = express();
 const port = process.env.PORT || 3001;
 
 //middlewares
